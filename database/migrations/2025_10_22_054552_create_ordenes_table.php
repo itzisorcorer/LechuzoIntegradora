992089b271b651410664bdb32ff791322bdab8ee
Alan Camacho <alancamacho491@gmail.com>
2025-10-22 00:12:50 -0600

    Implementados todos los enums

--- database/migrations/2025_10_22_054552_create_ordenes_table.php
+++ database/migrations/2025_10_22_054552_create_ordenes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('ordenes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->enum('status', ['pendiente', 'confirmado', 'en_proceso', 'listo', 'completado', 'cancelado'])->default('pendiente');
         });
     }
 
--- database/migrations/2025_10_22_054617_create_pagos_table.php
+++ database/migrations/2025_10_22_054617_create_pagos_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('pagos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->enum('status', ['pendiente', 'confirmado', 'fallido'])->default('pendiente');
         });
     }
 
